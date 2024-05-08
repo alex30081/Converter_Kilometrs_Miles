0256329e612e22ed57536291e9894f03329f16da
Boichenko Oleksandr <alex.homerepair.www@gmail.com>
2024-05-08 06:47:06 +0100

    Add Miles to kilometrs convert

--- src/app/Main.java
+++ src/app/Main.java
@@ -1,8 +1,21 @@
 package app;
 
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Convert Miles to kilometrs App");
+public class Main  {
+
+    private static final double CONV_ML = 1.60934;        //  1 Miles = 1.60934 Km.
+
+
+        public static void main(String[] args) {
+            System.out.println("Convert Miles to kilometrs App");
+
+            double ml = 15;
+            double miles = convMlToKm(ml);
+            System.out.println("Result is " + miles + " miles.");
+        }
+
+        private static double convMlToKm(double ml) {
+            return  ml * CONV_ML;
+        }
+
 
     }
-}
